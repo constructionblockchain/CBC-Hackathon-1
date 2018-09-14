11bbe458309eccf1e65cd5c7d7a448abd28c4af2
Sebastian Windeck <sebastian.windeck@web.de>
2018-09-14 17:56:44 +0200

    Create PayFlow

--- cordapp/src/main/kotlin/com/template/PayFlow.kt
+++ cordapp/src/main/kotlin/com/template/PayFlow.kt
@@ -1,57 +1,76 @@
 package com.template
 
 import co.paralleluniverse.fibers.Suspendable
+import net.corda.confidential.IdentitySyncFlow
 import net.corda.core.contracts.Command
 import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.flows.*
-import net.corda.core.identity.Party
 import net.corda.core.node.services.queryBy
 import net.corda.core.node.services.vault.QueryCriteria
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
-import java.lang.IllegalStateException
+import net.corda.finance.contracts.asset.Cash
 
-// *********
-// * Flows *
-// *********
 @InitiatingFlow
 @StartableByRPC
-class PayFlow(val linearId: UniqueIdentifier) : FlowLogic<SignedTransaction>() {
+class PayFlow(private val linearId: UniqueIdentifier) : FlowLogic<SignedTransaction>() {
 
     override val progressTracker = ProgressTracker()
 
     @Suspendable
     override fun call(): SignedTransaction {
+
         val criteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(linearId))
         val results = serviceHub.vaultService.queryBy<JobState>(criteria)
         val inputStateAndRef = results.states.single()
         val inputState = inputStateAndRef.state
 
-        if (inputState.data.developer != ourIdentity) throw IllegalStateException("Developer must start this flow.")
+        // Stage 3. This flow can only be initiated by the current recipient.
 
-        val jobState = inputState.data.copy(status = JobStatus.COMPLETED)
+        check(inputState.data.developer == ourIdentity) {
+            throw FlowException("Payment flow must be initiated by the developer.")
+        }
 
-        val payJobCommand = Command(
-                JobContract.Commands.FinishJob(),
-                listOf(ourIdentity.owningKey))
+        // Stage 5. Create a settle command.
+
+        val payCommand = Command(
+                JobContract.Commands.Pay(),
+                inputState.data.participants.map { it.owningKey }
+        )
 
         val transactionBuilder = TransactionBuilder(inputState.notary)
-                .addOutputState(jobState, JobContract.ID)
-                .addCommand(payJobCommand)
+                .addInputState(inputStateAndRef)
+                .addCommand(payCommand)
 
-        val signedTransaction =
-                serviceHub.signInitialTransaction(transactionBuilder)
 
-        return subFlow(FinalityFlow(signedTransaction))
+
+
+        // Stage 7. Get some cash from the vault and add a spend to our transaction builder.
+        // We pay cash to the lenders obligation key.
+        val contractor = inputState.data.contractor
+        val (_, cashSigningKeys) = Cash.generateSpend(serviceHub, transactionBuilder, inputState.data.amount, contractor)
+
+        transactionBuilder.verify(serviceHub)
+        val ptx = serviceHub.signInitialTransaction(transactionBuilder, cashSigningKeys + ourIdentity.owningKey)
+
+        val session = initiateFlow(ourIdentity)
+        subFlow(IdentitySyncFlow.Send(session, ptx.tx))
+        val stx = subFlow(CollectSignaturesFlow(
+                ptx,
+                setOf(session),
+                cashSigningKeys)
+        )
+
+        return subFlow(FinalityFlow(stx))
     }
 }
 
 @InitiatedBy(PayFlow::class)
-class PayFlowResponder(val developerSession: FlowSession) : FlowLogic<Unit>() {
+class PayFlowResponder(private val otherFlow: FlowSession) : FlowLogic<Unit>() {
     @Suspendable
     override fun call() {
-        class OurSignTransactionFlow : SignTransactionFlow(developerSession) {
+        class OurSignTransactionFlow : SignTransactionFlow(otherFlow) {
             override fun checkTransaction(stx: SignedTransaction) {
 
             }
@@ -59,5 +78,4 @@ class PayFlowResponder(val developerSession: FlowSession) : FlowLogic<Unit>() {
 
         subFlow(OurSignTransactionFlow())
     }
-
 }
