14ffc4a8f63193840b343fe629c8309e8dd609e0
Hristo Gatsinski <h.gatsinski@magicsolutions.bg>
2018-09-16 21:36:40 +0300

    Refactor static files

--- server/src/main/resources/static/app.js
+++ server/src/main/resources/static/app.js
@@ -1,3 +1,24 @@
 "use strict";
 
 // Define your client-side logic here.
+<script type="text/javascript">
+$('form').submit(function(e) {
+    var $form = $(this);
+    var url = $form.attr('action');
+
+    $.ajax({
+           type: 'POST',
+           url: url,
+           data: $form.serialize(),
+           success: function(data) {
+               $('div.alert').remove();
+               $form.after('<br><div class="alert alert-success" role="alert">' + data + '</div>');
+           },
+           error: function(data) {
+               $('div.alert').remove();
+               $form.after('<br><div class="alert alert-danger" role="alert">Invalid input!</div>');
+           }
+         });
+
+    e.preventDefault();
+});
