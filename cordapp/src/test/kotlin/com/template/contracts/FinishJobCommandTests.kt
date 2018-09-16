e9448a0edbb71962ee61f41a212021194b9d86d4
Hristo Gatsinski <h.gatsinski@magicsolutions.bg>
2018-09-16 22:19:47 +0300

    Change `FinishJobCommandTests`

--- cordapp/src/test/kotlin/com/template/contracts/FinishJobCommandTests.kt
+++ cordapp/src/test/kotlin/com/template/contracts/FinishJobCommandTests.kt
@@ -1,121 +1,146 @@
-//package com.template.contracts
-//
-//import com.template.JobContract
-//import com.template.JobState
-//import com.template.JobStatus
-//import net.corda.core.identity.CordaX500Name
-//import net.corda.testing.core.TestIdentity
-//import net.corda.testing.node.MockServices
-//import net.corda.testing.node.ledger
-//import org.junit.Test
-//
-//class FinishJobCommandTests {
-//    private val ledgerServices = MockServices(listOf("com.template"))
-//    private val developer = TestIdentity(CordaX500Name("John Doe", "City", "GB"))
-//    private val contractor = TestIdentity(CordaX500Name("Richard Roe", "Town", "GB"))
-//    private val thirdParty = TestIdentity(CordaX500Name("John Roe", "Town", "GB"))
-//    private val participants = listOf(developer.publicKey, contractor.publicKey)
-//    private val startedJobState = JobState(
-//        description = "Job description",
-//        status = JobStatus.STARTED,
-//        developer = developer.party,
-//        contractor = contractor.party
-//    )
-//    private val completedJobState = startedJobState.copy(status = JobStatus.COMPLETED)
-//
-//    @Test
-//    fun `FinishJob command should complete successfully`() {
-//        ledgerServices.ledger {
-//            transaction {
-//                command(participants, JobContract.Commands.FinishJob())
-//                input(JobContract.ID, startedJobState)
-//                output(JobContract.ID, completedJobState)
-//                verifies()
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun `One input should be consumed`() {
-//        ledgerServices.ledger {
-//            transaction {
-//                command(participants, JobContract.Commands.FinishJob())
-//                output(JobContract.ID, completedJobState)
-//                failsWith("one input should be consumed")
-//            }
-//            transaction {
-//                command(participants, JobContract.Commands.FinishJob())
-//                input(JobContract.ID, startedJobState)
-//                input(JobContract.ID, startedJobState)
-//                output(JobContract.ID, completedJobState)
-//                failsWith("one input should be consumed")
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun `One output should be produced`() {
-//        ledgerServices.ledger {
-//            transaction {
-//                command(participants, JobContract.Commands.FinishJob())
-//                input(JobContract.ID, startedJobState)
-//                failsWith("one output should be produced")
-//            }
-//            transaction {
-//                command(participants, JobContract.Commands.FinishJob())
-//                input(JobContract.ID, startedJobState)
-//                output(JobContract.ID, completedJobState)
-//                output(JobContract.ID, completedJobState)
-//                failsWith("one output should be produced")
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun `The input status must be set as STARTED`() {
-//        ledgerServices.ledger {
-//            transaction {
-//                command(participants, JobContract.Commands.FinishJob())
-//                input(JobContract.ID, completedJobState)
-//                output(JobContract.ID, completedJobState)
-//                failsWith("the input status must be set as started")
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun `The output status should be set as FINISHED`() {
-//        ledgerServices.ledger {
-//            transaction {
-//                command(participants, JobContract.Commands.FinishJob())
-//                input(JobContract.ID, startedJobState)
-//                output(JobContract.ID, startedJobState)
-//                failsWith("the output status should be set as finished")
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun `Only the status should change`() {
-//        ledgerServices.ledger {
-//            transaction {
-//                command(participants, JobContract.Commands.FinishJob())
-//                input(JobContract.ID, startedJobState)
-//                output(JobContract.ID, completedJobState.copy(developer = thirdParty.party))
-//                failsWith("only the status must change")
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun `The contractor should be a signer of the transaction`() {
-//        ledgerServices.ledger {
-//            transaction {
-//                command(listOf(developer.publicKey), JobContract.Commands.FinishJob())
-//                input(JobContract.ID, startedJobState)
-//                output(JobContract.ID, completedJobState)
-//                failsWith("the contractor should be signer")
-//            }
-//        }
-//    }
-//}
+package com.template.contracts
+
+import com.template.JobContract
+import com.template.JobState
+import com.template.Milestone
+import com.template.MilestoneStatus
+import net.corda.core.identity.CordaX500Name
+import net.corda.finance.DOLLARS
+import net.corda.testing.core.TestIdentity
+import net.corda.testing.node.MockServices
+import net.corda.testing.node.ledger
+import org.junit.Test
+
+class FinishJobCommandTests {
+    private val ledgerServices = MockServices(listOf("com.template"))
+    private val developer = TestIdentity(CordaX500Name("John Doe", "City", "GB"))
+    private val contractor = TestIdentity(CordaX500Name("Richard Roe", "Town", "GB"))
+    private val participants = listOf(contractor.publicKey)
+    private val startedMilestone = Milestone("Fit windows.", 100.DOLLARS, MilestoneStatus.STARTED)
+    private val completedMilestone = startedMilestone.copy(status = MilestoneStatus.COMPLETED)
+    private val otherMilestone = Milestone("Fit doors", 50.DOLLARS)
+    private val startedJobState = JobState(
+        milestones = listOf(startedMilestone, otherMilestone),
+        developer = developer.party,
+        contractor = contractor.party
+    )
+    private val completedJobState = startedJobState.copy(
+        milestones = listOf(completedMilestone, otherMilestone))
+
+    @Test
+    fun `FinishMilestone command should complete successfully`() {
+        ledgerServices.ledger {
+            transaction {
+                command(participants, JobContract.Commands.FinishMilestone(0))
+                input(JobContract.ID, startedJobState)
+                output(JobContract.ID, completedJobState)
+                verifies()
+            }
+        }
+    }
+
+    @Test
+    fun `One JobState input should be consumed`() {
+        ledgerServices.ledger {
+            transaction {
+                command(participants, JobContract.Commands.FinishMilestone(0))
+                output(JobContract.ID, completedJobState)
+                failsWith("One JobState input should be consumed.")
+            }
+            transaction {
+                command(participants, JobContract.Commands.FinishMilestone(0))
+                input(JobContract.ID, startedJobState)
+                input(JobContract.ID, startedJobState)
+                output(JobContract.ID, completedJobState)
+                failsWith("One JobState input should be consumed.")
+            }
+        }
+    }
+
+    @Test
+    fun `One JobState output should be produced`() {
+        ledgerServices.ledger {
+            transaction {
+                command(participants, JobContract.Commands.FinishMilestone(0))
+                input(JobContract.ID, startedJobState)
+                failsWith("One JobState output should be produced")
+            }
+            transaction {
+                command(participants, JobContract.Commands.FinishMilestone(0))
+                input(JobContract.ID, startedJobState)
+                output(JobContract.ID, completedJobState)
+                output(JobContract.ID, completedJobState)
+                failsWith("One JobState output should be produced.")
+            }
+        }
+    }
+
+    @Test
+    fun `The modified milestone should have an input status of STARTED`() {
+        ledgerServices.ledger {
+            transaction {
+                command(participants, JobContract.Commands.FinishMilestone(0))
+                input(JobContract.ID, completedJobState)
+                output(JobContract.ID, completedJobState)
+                failsWith("The modified milestone should have an input status of STARTED.")
+            }
+        }
+    }
+
+    @Test
+    fun `The modified milestone should have an output status of COMPLETED`() {
+        ledgerServices.ledger {
+            transaction {
+                command(participants, JobContract.Commands.FinishMilestone(0))
+                input(JobContract.ID, startedJobState)
+                output(JobContract.ID, startedJobState)
+                failsWith("The modified milestone should have an output status of COMPLETED.")
+            }
+        }
+    }
+
+    @Test
+    fun `The modified milestone's description and amount shouldn't change`() {
+        ledgerServices.ledger {
+            transaction {
+                command(participants, JobContract.Commands.FinishMilestone(0))
+                input(JobContract.ID, startedJobState)
+                output(JobContract.ID, completedJobState.copy(
+                    milestones = listOf(completedMilestone.copy(description = "Changed milestone description"))))
+                failsWith("The modified milestone's description and amount shouldn't change.")
+            }
+            transaction {
+                command(participants, JobContract.Commands.FinishMilestone(0))
+                input(JobContract.ID, startedJobState)
+                output(JobContract.ID, completedJobState.copy(
+                    milestones = listOf(completedMilestone.copy(amount = 200.DOLLARS))))
+                failsWith("The modified milestone's description and amount shouldn't change.")
+            }
+        }
+    }
+
+    @Test
+    fun `All the other milestones should be unmodified`() {
+        ledgerServices.ledger {
+            transaction {
+                command(participants, JobContract.Commands.FinishMilestone(0))
+                input(JobContract.ID, startedJobState)
+                output(JobContract.ID, completedJobState.copy(
+                    milestones = listOf(completedMilestone, otherMilestone.copy(amount = 200.DOLLARS))))
+                failsWith("All the other milestones should be unmodified.")
+            }
+        }
+    }
+    
+    @Test
+    fun `The contractor should be a required signer`() {
+        ledgerServices.ledger {
+            transaction {
+                command(listOf(developer.publicKey), JobContract.Commands.FinishMilestone(0))
+                input(JobContract.ID, startedJobState)
+                output(JobContract.ID, completedJobState)
+                failsWith("The contractor should be a required signer.")
+            }
+        }
+    }
+}
